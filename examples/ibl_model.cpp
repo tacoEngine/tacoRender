3a29641aeaede26280f207966d706ff56c1285b2
Not-Nik <nik.wipper@gmx.de>
2024-12-17 20:26:54 +0100

    Add IBL model example

---
+++ examples/ibl_model.cpp
@@ -0,0 +1,82 @@
+// rl3d (c) Nikolas Wipper 2023
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include <cstdio>
+#include <rl3d.h>
+#include <rl3d_effects.h>
+
+#include "raymath.h"
+
+int main() {
+    SetTargetFPS(60);
+
+    const int screenWidth = 500, screenHeight = 500;
+
+    InitWindow(screenWidth, screenHeight, "rl3d emissions sphere");
+    Init3D();
+
+    Camera3D camera = (Camera3D){
+        .position = (Vector3){0, 2, -5}, .target = (Vector3){0, 0, 0}, .up = (Vector3){
+            0, 1, 0
+        },
+        .fovy = 72.f, .projection = CAMERA_PERSPECTIVE
+    };
+
+    Model model = LoadModel("examples/assets/model/DamagedHelmet.glb");
+
+    TextureCubemap radiance = LoadTextureCubemap(LoadImage("examples/assets/ibl/radiance.dds"), CUBEMAP_LAYOUT_AUTO_DETECT);
+    TextureCubemap irradiance = LoadTextureCubemap(LoadImage("examples/assets/ibl/irradiance.hdr"), CUBEMAP_LAYOUT_AUTO_DETECT);
+
+    GBuffers buffers = LoadGBuffers(screenWidth, screenHeight);
+    GBufferPresenter presenter = LoadPresenter(buffers);
+
+    model.materials[0].shader = GetGBufferShader();
+    model.materials[1].shader = GetGBufferShader();
+
+    while (!WindowShouldClose()) {
+        UpdateCamera(&camera, CAMERA_ORBITAL);
+
+        { // Render to GBuffers
+            BeginGBufferMode(buffers);
+
+            ClearBackground(BLACK);
+
+            BeginMode3D(camera);
+
+            DrawModel(model, (Vector3){0, 0, 0}, 1.f, WHITE);
+
+            EndMode3D();
+
+            EndGBufferMode();
+        }
+
+        ClearPresenter(presenter);
+
+        { // Shade scene
+            BeginLightingPass(presenter);
+
+            ClearBackground(BLACK);
+
+            LightSun(presenter, camera, (Vector3){0, -1, -1}, 1, WHITE, NULL_SHADOW_MAP);
+            LightIBL(presenter, camera, radiance, irradiance);
+
+            EndLightingPass();
+        }
+
+        AddBackBuffer(presenter);
+
+        ApplyToneMapping(presenter, TONE_MAP_REINHARD);
+        ApplyGammaCorrection(presenter, 2.2);
+
+        // Render GBuffers to screen
+        Present(presenter);
+    }
+
+    Uninit3D();
+    CloseWindow();
+}
