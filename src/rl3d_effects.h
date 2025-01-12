2d9155b9fdb8c2e4996468a2210ca010b34e0e98
Nikolas <nik.wipper@gmx.de>
2025-01-12 22:49:46 +0100

    Remove bloom

--- src/rl3d_effects.h
+++ src/rl3d_effects.h
@@ -61,7 +61,6 @@ void DrawSkybox(Skybox skybox, Color tint);
 
 void ShadeFlat(GBufferPresenter presenter);
 
-void ApplyBloom(GBufferPresenter presenter, unsigned int iterations, float radius);
 void ApplyGammaCorrection(GBufferPresenter presenter, float gamma);
 void ApplyToneMapping(GBufferPresenter presenter, ToneMapper mapper);
 void ApplySSAO(GBufferPresenter presenter, Camera camera);
