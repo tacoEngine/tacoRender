f5b96e263688abd6aa7fb8e60ef77772789e0620
Nikolas <nik.wipper@gmx.de>
2025-05-06 16:18:20 +0200

    Increase depth display shader range

--- src/shaders/depth_display.fs.glsl
+++ src/shaders/depth_display.fs.glsl
@@ -8,10 +8,11 @@ uniform sampler2D depth;
 
 float linearizeDepth(float d, float zNear, float zFar) {
     float z_n = 2.0 * d - 1.0;
-    return 2.0 * zNear * zFar / (zFar + zNear - z_n * (zFar - zNear));
+    float lin = 2.0 * zNear * zFar / (zFar + zNear - z_n * (zFar - zNear));
+    return (lin - zNear) / (zFar - zNear);
 }
 
 void main() {
     float depth = texture(depth, fragTexCoord).r;
-    finalColor = vec4(vec3(linearizeDepth(depth, 0.01, 1.0)), 1);
+    finalColor = vec4(vec3(linearizeDepth(depth, 0.01, 10.0)), 1);
 }
