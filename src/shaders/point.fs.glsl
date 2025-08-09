3a28f553ebda9c3d3fe3893428a509fd026a51c2
Nikolas <nik.wipper@gmx.de>
2025-08-09 20:44:04 +0200

    Move from Lambert to Bureley/Disney Diffuse

--- src/shaders/point.fs.glsl
+++ src/shaders/point.fs.glsl
@@ -42,11 +42,11 @@ vec3 WorldPosFromDepth(float depth) {
 float DistributionGGX(vec3 normal, vec3 H, float roughness) {
     float a      = roughness*roughness;
     float a2     = a*a;
-    float NdotH  = max(dot(normal, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
+    float hDotN  = max(dot(H, normal), 0.0);
+    float hDotN2 = hDotN*hDotN;
 
     float num   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    float denom = (hDotN2 * (a2 - 1.0) + 1.0);
     denom = PI * denom * denom;
 
     return num / denom;
@@ -62,17 +62,20 @@ float GeometrySchlickGGX(float NdotV, float roughness) {
     return num / denom;
 }
 
-float GeometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float roughness) {
-    float NdotV = max(dot(normal, viewDir), 0.0);
-    float NdotL = max(dot(normal, lightDir), 0.0);
-    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+float GeometrySmith(float vDotN, float lDotN, float roughness) {
+    float ggx2  = GeometrySchlickGGX(vDotN, roughness);
+    float ggx1  = GeometrySchlickGGX(lDotN, roughness);
 
     return ggx1 * ggx2;
 }
 
-vec3 FresnelSchlick(float cosTheta, vec3 F0) {
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+vec3 FresnelSchlick(float vDotH, vec3 F0) {
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - vDotH, 0.0, 1.0), 5.0);
+}
+
+vec3 BurleyDiffuse(vec3 albedo, float lDotN, float vDotN, float vDotH, float roughness) {
+    float F90 = 0.5 + 2 * roughness * vDotH;
+    return albedo / PI * (1 + (F90 - 1) * pow(1 - lDotN, 5.0)) * (1 + (F90 - 1) * pow(1 - vDotN, 5.0));
 }
 
 void main() {
@@ -92,10 +95,11 @@ void main() {
 
     vec3 lightDir = normalize(pos - worldPos);
     vec3 viewDir = normalize(camPos - worldPos);
-
-    float lightAngle = max(dot(lightDir, normal), 0.0);
-
     vec3 halfwayDir = normalize(lightDir + viewDir);
+
+    float lDotN = max(dot(lightDir, normal), 0.0);
+    float vDotN = max(dot(viewDir, normal), 0.0);
+    float vDotH = max(dot(viewDir, halfwayDir), 0.0);
 
     float att = pow(clamp(1 - pow(lightDistance/radius, 4), 0.0, 1.0), 2) / (lightDistance * lightDistance + 1);
 
@@ -103,21 +107,23 @@ void main() {
 
     // cook-torrance brdf
     float NDF = DistributionGGX(normal, halfwayDir, roughness);
-    float G = GeometrySmith(normal, viewDir, lightDir, roughness);
+    float G = GeometrySmith(vDotN, lDotN, roughness);
 
     vec3 F0 = vec3(0.04);
     F0 = mix(F0, albedo.rgb, metallic);
-    vec3 F = FresnelSchlick(max(dot(halfwayDir, viewDir), 0.0), F0);
+    vec3 F = FresnelSchlick(vDotH, F0);
 
     vec3 kS = F;
     vec3 kD = vec3(1.0) - kS;
     kD *= 1.0 - metallic;
 
     vec3 numerator = NDF * G * F;
-    float denominator = 4.0 * max(dot(normal, viewDir), 0.0) * lightAngle + 0.0001;
+    float denominator = 4.0 * vDotN * lDotN + 0.0001;
     vec3 specular = numerator / denominator;
 
-    vec3 outColor = (kD * albedo.rgb / PI + specular) * radiance * lightAngle;
+    vec3 diffuse = BurleyDiffuse(albedo.rgb, lDotN, vDotN, vDotH, roughness);
+
+    vec3 outColor = (kD * diffuse + specular) * radiance * lDotN;
 
     finalColor = vec4(outColor + emission, albedo.a);
 }
--- src/shaders/sun.fs.glsl
+++ src/shaders/sun.fs.glsl
@@ -47,11 +47,11 @@ vec3 WorldPosFromDepth(float depth) {
 float DistributionGGX(vec3 normal, vec3 H, float roughness) {
     float a      = roughness*roughness;
     float a2     = a*a;
-    float NdotH  = max(dot(normal, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
+    float hDotN  = max(dot(H, normal), 0.0);
+    float hDotN2 = hDotN*hDotN;
 
     float num   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    float denom = (hDotN2 * (a2 - 1.0) + 1.0);
     denom = PI * denom * denom;
 
     return num / denom;
@@ -67,17 +67,20 @@ float GeometrySchlickGGX(float NdotV, float roughness) {
     return num / denom;
 }
 
-float GeometrySmith(vec3 normal, vec3 viewDir, vec3 lightDir, float roughness) {
-    float NdotV = max(dot(normal, viewDir), 0.0);
-    float NdotL = max(dot(normal, lightDir), 0.0);
-    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+float GeometrySmith(float vDotN, float lDotN, float roughness) {
+    float ggx2  = GeometrySchlickGGX(vDotN, roughness);
+    float ggx1  = GeometrySchlickGGX(lDotN, roughness);
 
     return ggx1 * ggx2;
 }
 
-vec3 FresnelSchlick(float cosTheta, vec3 F0) {
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+vec3 FresnelSchlick(float vDotH, vec3 F0) {
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - vDotH, 0.0, 1.0), 5.0);
+}
+
+vec3 BurleyDiffuse(vec3 albedo, float lDotN, float vDotN, float vDotH, float roughness) {
+    float F90 = 0.5 + 2 * roughness * vDotH;
+    return albedo / PI * (1 + (F90 - 1) * pow(1 - lDotN, 5.0)) * (1 + (F90 - 1) * pow(1 - vDotN, 5.0));
 }
 
 float SampleCascade(int cascadeIndex, vec3 coords) {
@@ -131,8 +134,11 @@ void main() {
 
     vec3 lightDir = normalize(-direction);
     vec3 viewDir = normalize(camPos - worldPos);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
 
-    float lightAngle = max(dot(lightDir, normal), 0.0);
+    float lDotN = max(dot(lightDir, normal), 0.0);
+    float vDotN = max(dot(viewDir, normal), 0.0);
+    float vDotH = max(dot(viewDir, halfwayDir), 0.0);
 
     float shadowFactor = 1.0;
 
@@ -144,27 +150,27 @@ void main() {
         }
     }
 
-    vec3 halfwayDir = normalize(lightDir + viewDir);
-
     vec3 radiance = color * intensity;
 
     // cook-torrance brdf
     float NDF = DistributionGGX(normal, halfwayDir, roughness);
-    float G = GeometrySmith(normal, viewDir, lightDir, roughness);
+    float G = GeometrySmith(vDotN, lDotN, roughness);
 
     vec3 F0 = vec3(0.04);
     F0 = mix(F0, albedo.rgb, metallic);
-    vec3 F = FresnelSchlick(max(dot(halfwayDir, viewDir), 0.0), F0);
+    vec3 F = FresnelSchlick(vDotH, F0);
 
     vec3 kS = F;
     vec3 kD = vec3(1.0) - kS;
     kD *= 1.0 - metallic;
 
     vec3 numerator = NDF * G * F;
-    float denominator = 4.0 * max(dot(normal, viewDir), 0.0) * lightAngle + 0.0001;
+    float denominator = 4.0 * vDotN * lDotN + 0.0001;
     vec3 specular = numerator / denominator;
 
-    vec3 outColor = (kD * albedo.rgb / PI + specular) * radiance * lightAngle;
+    vec3 diffuse = BurleyDiffuse(albedo.rgb, lDotN, vDotN, vDotH, roughness);
+
+    vec3 outColor = (kD * diffuse + specular) * radiance * lDotN;
 
     finalColor = vec4(outColor * shadowFactor + emission, albedo.a);
 }
